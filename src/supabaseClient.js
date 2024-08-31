91a5f5782c3f4446656f9b19739bbc8f55d5652b
Agbasi Stephanie <agbasistephanie@Agbasis-MacBook-Pro.local>
2024-08-31 23:55:09 +0100

    env created

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,7 @@
-
-
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dlwxcppypoesugpvbhdd.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsd3hjcHB5cG9lc3VncHZiaGRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2Mzg2MTUsImV4cCI6MjA0MDIxNDYxNX0.OGaBycD3vEO8wwvYX_4wsyNq8lu6TsFZLwtriD7ybxo';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
